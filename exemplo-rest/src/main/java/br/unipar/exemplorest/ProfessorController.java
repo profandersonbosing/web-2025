8d21ae6ac0cc6f5e4046bd47ee4774951b1cee52
profandersonbosing <110434492+profandersonbosing@users.noreply.github.com>
2025-04-30 19:56:27 -0300

    Add files via upload

--- exemplo-rest/src/main/java/br/unipar/exemplorest/ProfessorController.java
+++ exemplo-rest/src/main/java/br/unipar/exemplorest/ProfessorController.java
@@ -10,9 +10,17 @@ import java.util.List;
 @Path("/professores")
 public class ProfessorController {
 
+    //C - @POST - /professores - Body
+    //R - @GET - /professores?nomeContem=And, /professores/{id}
+    //U - @PUT - /professores/{id} - Body
+    //D - @DELETE - /professores/{id}
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Professor> findAll() {
+    public List<Professor> findAll(@QueryParam("nomeContem") String nomeContem,
+                               @QueryParam("salario") Double salario) {
+        System.out.println(nomeContem);
+        System.out.println(salario);
         ArrayList<Professor> professores = new ArrayList<>();
         professores.add(new Professor(1, "Professor Anderson"));
         professores.add(new Professor(2, "Professor Triacca"));
@@ -32,6 +40,21 @@ public class ProfessorController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Professor insert(Professor professor) {
+        return professor;
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") Integer id) {
+        // Implementar a lógica de exclusão
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Professor update(@PathParam("id") Integer id, Professor professor) {
+        // Implementar a lógica de atualização
         return professor;
     }
 
