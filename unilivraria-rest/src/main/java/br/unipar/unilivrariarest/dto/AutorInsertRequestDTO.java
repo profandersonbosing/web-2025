52d72060c0cf2e9b544b9c8fff8dee589d176239
profandersonbosing <110434492+profandersonbosing@users.noreply.github.com>
2025-04-30 20:52:03 -0300

    Add files via upload

---
+++ unilivraria-rest/src/main/java/br/unipar/unilivrariarest/dto/AutorInsertRequestDTO.java
@@ -0,0 +1,14 @@
+package br.unipar.unilivrariarest.dto;
+
+public class AutorInsertRequestDTO {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
